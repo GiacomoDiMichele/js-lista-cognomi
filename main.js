a791ec2bf200648f5759eec4f87caa71567d4e04
Giacomo <giacomo.1936@gmail.com>
2020-10-19 15:55:24 +0200

    impostato array in ordine alfabetico e indicata posizione cognome

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 //creo un array con vari cognomi
 var lista_cognomi = ['rossi', 'di carlo', 'ghersi',];
-//imposto i cognomi in ordine casuale
+
+//chiedo il cognome dell'utente attuale e lo pusho nell'array dei cognomi già inseriti
 var cognome_corrente = prompt("inserisci il tuo cognome")
     lista_cognomi.push(cognome_corrente);
+
+//imposto tutti i cognomi in ordine casuale
 lista_cognomi.sort(() => Math.random() - 0.5);
-//stampo i cognomi per verificare che siano continuamente in ordine casuale
+
+//stampo i cognomi per verificare che siano continuamente in ordine casuale, compreso quello inserito in seguito
 console.log(lista_cognomi);
+
+//genero i cognomi in ordine alfabetico
+lista_cognomi.sort();
+//verifico che siano in ordine alfabetico
+console.log(lista_cognomi);
+
+//trovo la posizione del cognome "di michele" nell'array in ordine alfabetico e la stampo
+console.log(lista_cognomi.indexOf('di michele'));
